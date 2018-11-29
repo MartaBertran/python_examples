050837bb73f1a9dabda5758710ba5d923d56aa64
marta.bertran <martabertrangarcia@gmail.com>
2018-11-29 13:29:04 +0100

    Update requests_example.py

--- requests_example.py
+++ requests_example.py
@@ -1,1 +1,16 @@
-#Requests review
+#REQUESTS REVIEW
+
+# import the required module for the html requests
+import requests
+
+# a Response object, called r, needs to be created 
+r = requests.get()
+# read the content of the server's response
+r.text
+
+
+
+#INFO LINKS
+# https://jupyter-kernel-gateway.readthedocs.io/en/latest/http-mode.html
+# http://docs.python-requests.org/en/master/
+# http://docs.python-requests.org/en/master/user/quickstart/#binary-response-content
